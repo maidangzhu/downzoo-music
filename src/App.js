aa9a62a57a4eb46d8074227b9ad2a35a7f4af569
麦当 <borisdunk@sina.com>
2021-10-25 22:44:57 +0800

    feat(downzoo-music): add iconStyle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store';
-import {GlobalStyle} from './style';
+
 import {renderRoutes} from 'react-router-config';
-import {HashRouter} from 'react-router-dom';
 import routes from './routes';
+
+import { IconStyle } from "./assets/iconfont/iconfont";
+import {GlobalStyle} from './style';
 
 function App() {
   return (
     <Provider store={store}>
       <HashRouter>
         <GlobalStyle/>
+        <IconStyle />
         {renderRoutes(routes)}
       </HashRouter>
     </Provider>
